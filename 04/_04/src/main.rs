0812acfb82b39c548c2050ddb7df65026264159d
Gvstavo <gustavopaivads@gmail.com>
2021-04-29 23:50:22 -0300

    > iniciando

--- 04/_04/src/main.rs
+++ 04/_04/src/main.rs
@@ -1,4 +1,23 @@
-fn main() {
+#[macro_use]
+extern crate actix_web;
 
+use actix_web::{web, App , HttpResponse, HttpServer};
+use actix_files::Files;
+use serde::{Deserialize, Serialize};
 
+mod response;
+use response::Response;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+	println!("Listening on port 8080");
+	HttpServer::new(|| {
+		App::new()
+		.service(Files::new("/","static").index_file("index.html"))
+	})
+	.bind("127.0.0.1:8080")?
+	.run()
+	.await
 }
+
