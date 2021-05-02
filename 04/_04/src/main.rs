eb519a4d2106bf7f8e9c899728286e10a4cf985a
Gvstavo <gustavopaivads@gmail.com>
2021-05-02 02:11:32 -0300

    > encoding with base64

--- 04/_04/src/main.rs
+++ 04/_04/src/main.rs
@@ -4,12 +4,18 @@ extern crate actix_web;
 use actix_web::{web, App , HttpResponse, HttpServer};
 use actix_files::Files;
 use serde::{Deserialize, Serialize};
-
+use openssl::rsa::*;
+use std::cmp::max;
+use rsa::{PublicKey, RSAPrivateKey, RSAPublicKey, PaddingScheme};
+use rand::rngs::OsRng;
 mod response;
 use response::Response;
 
 mod request;
 use request::Request;
+
+mod encrypt;
+use encrypt::*;
 
 #[post("/keys")]
 async fn key_pairs(t: web::Json<Request>)->HttpResponse {
@@ -20,6 +26,31 @@ async fn key_pairs(t: web::Json<Request>)->HttpResponse {
 	HttpResponse::Ok().json(ret)
 
 }
+#[post("/public_encrypt")]
+async fn public_encrypt(t: web::Json<Encrypt>)->HttpResponse {
+
+	println!("encrypting...");
+	println!("{:?}", t);
+
+	let u = t.public_key.as_bytes();
+
+	let data = t.plain_text.as_bytes();
+	println!("{:?}",data );
+	let k = Rsa::public_key_from_pem(u).unwrap();
+
+	let mut buffer : Vec<u8> = vec![0; k.size() as usize];
+	k.public_encrypt(data, &mut buffer, Padding::PKCS1).unwrap();
+			
+
+	let ret = ResponseEncrypt{
+		text: base64::encode(buffer)
+	};
+
+	HttpResponse::Ok().json(ret)
+
+
+}
+
 
 
 #[actix_web::main]
@@ -28,18 +59,12 @@ async fn main() -> std::io::Result<()> {
 	println!("Listening on port 8080");
 	HttpServer::new(|| {
 		App::new()
-		.service(Files::new("/","static").index_file("index.html"))
 		.service(key_pairs)
+		.service(public_encrypt)
+		.service(Files::new("/","static").index_file("index.html"))
 	})
 	.bind("127.0.0.1:8080")?
 	.run()
 	.await
 }
 
-
-#[test]
-fn name() {
-	let keys = Response::new(2048);
-
-	println!("{:?}",keys );
-}
