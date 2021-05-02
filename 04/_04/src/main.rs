4e437186ae1b5268772c8baf5b5fcc75cc37abe0
Gvstavo <gustavopaivads@gmail.com>
2021-05-01 23:31:30 -0300

    > backend geenrating keys

--- 04/_04/src/main.rs
+++ 04/_04/src/main.rs
@@ -8,6 +8,20 @@ use serde::{Deserialize, Serialize};
 mod response;
 use response::Response;
 
+mod request;
+use request::Request;
+
+#[post("/keys")]
+async fn key_pairs(t: web::Json<Request>)->HttpResponse {
+
+	println!("generating keys...");
+	let ret = Response::new(t.n);
+
+	HttpResponse::Ok().json(ret)
+
+}
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
@@ -15,9 +29,17 @@ async fn main() -> std::io::Result<()> {
 	HttpServer::new(|| {
 		App::new()
 		.service(Files::new("/","static").index_file("index.html"))
+		.service(key_pairs)
 	})
 	.bind("127.0.0.1:8080")?
 	.run()
 	.await
 }
 
+
+#[test]
+fn name() {
+	let keys = Response::new(2048);
+
+	println!("{:?}",keys );
+}
